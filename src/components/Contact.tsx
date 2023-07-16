455409a1d3f80123b0114400083de73a6c98dec1
DataCodeLiteracy <dataliteracy@icloud.com>
2023-07-17 02:17:13 +0900

    feat: Contact 섹션 - 이미지, 전체적으로 중앙 정렬
    * 이미지
      - Contact 섹션 페이지 전체가 밋밋해보여서 이미지 추가
    
    * 중앙 정렬
      - 이미지를 추가하면서 전체적으로 중앙 정렬
    
    2023.07.17

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,24 +1,37 @@
 import React from 'react'
 import styles from '../app/page.module.css'
 import contactStyles from '../components/Contact.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Contact = () => {
   return (
     <section id="#about" className={`${styles.section} ${styles.contact}`}>
       <div className={contactStyles.container}>
         <h1>Contact</h1>
-        <div className={contactStyles.list}>
-          <div>
-            <button>Email</button>
-            <div>dataliteracy@icloud.com</div>
-          </div>
-          <div>
-            <button>GitHub</button>
-            <div>https://github.com/DataCodeLiteracy</div>
-          </div>
-          <div>
-            <button>Blog</button>
-            <div>https://velog.io/@dataliteracy</div>
+        <div className={contactStyles.listWrapper}>
+          <Image src="/Images/contactme.webp" alt="" width={400} height={400} />
+          <div className={contactStyles.list}>
+            <div>
+              <button>Email</button>
+              <div>dataliteracy@icloud.com</div>
+            </div>
+            <div>
+              <Link href="https://github.com/DataCodeLiteracy" target="_blank">
+                GitHub
+              </Link>
+              <Link href="https://github.com/DataCodeLiteracy" target="_blank">
+                https://github.com/DataCodeLiteracy
+              </Link>
+            </div>
+            <div>
+              <Link href="https://velog.io/@dataliteracy" target="_blank">
+                Blog
+              </Link>
+              <Link href="https://velog.io/@dataliteracy" target="_blank">
+                https://velog.io/@dataliteracy
+              </Link>
+            </div>
           </div>
         </div>
       </div>

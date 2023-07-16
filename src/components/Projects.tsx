0324aa2cc4d13f52693d7314cea97378ff826e37
DataCodeLiteracy <dataliteracy@icloud.com>
2023-07-17 01:47:12 +0900

    feat: Projects 섹션 - 이미지 등록, UI 수정하기
    * 이미지 등록
      - README에 사용한 gif 이미지 등록
    
    * UI 수정하기
      - 전체적으로 가운데 배치하고 이미지 영역을 키우고 제목과 설명의 색깔의 차이를 두었다.
      - 깃허브 링크 아이콘 추가
    
    2023.07.17

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import styles from '../app/page.module.css'
 import projectStyles from '../components/Projects.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
+import { AiFillGithub } from 'react-icons/ai'
 
 const Projects = () => {
   return (
@@ -8,14 +11,16 @@ const Projects = () => {
       <div className={projectStyles.container}>
         <h1>Projects</h1>
         <div className={projectStyles.projectsContainer}>
-          <div className={projectStyles.img}>이미지</div>
+          <Image className={projectStyles.img} src="/Images/todolist.gif" width={500} height={500} alt="" />
           <div className={projectStyles.list}>
             <h2>TODO LIST</h2>
             <p>
-              자바스크립트를 이용하여 만든 Todo List <br />내 첫 번쨰 토이 프로젝트이자, 바닐라 자바스크립트를 이용하여 만든 프로젝트 DOM API를 사용하여 처음으로 DOM을 조작해서
-              브라우저에서 내가 원하는 결과를 만들어낼 수 있다는 기쁨을 느꼈던 프로젝트
+              자바스크립트를 이용하여 만든 Todo List <br />내 첫 번쨰 토이 프로젝트이자, 바닐라 자바스크립트를 이용하여 만든 프로젝트
+              <br /> DOM API를 사용하여 처음으로 DOM을 조작해서 브라우저에서 내가 원하는 결과를 만들어낼 수 있다는 기쁨을 느꼈던 프로젝트
             </p>
-            <button>깃허브</button>
+            <Link href="https://github.com/DataCodeLiteracy/TodoList" target="_blank">
+              <AiFillGithub />
+            </Link>
           </div>
         </div>
       </div>
